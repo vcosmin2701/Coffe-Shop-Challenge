9befd2ca7f7ff041d71c642c1e0115f7c0107f67
vcosmin2701 <38065643+vcosmin2701@users.noreply.github.com>
2024-09-08 19:33:01 +0000

    [fix] - Coordinates range modified, no errors at this point

--- Validators/CoordinateValidator.swift
+++ Validators/CoordinateValidator.swift
@@ -2,8 +2,13 @@ protocol CoordinateValidator {
   func isValid(x: Double, y: Double) -> Bool
 }
 
+// Checked the max value of coordinates, and decided for this
+// implementation that x and also y can be arround this range [-180, 180]
+// for future implementation, maybe based on a real API, things
+// will be different
+
 class CoordinateValidatorImpl: CoordinateValidator{
   func isValid(x: Double, y: Double) -> Bool {
-    return x >= -180 && x <= 180 && y >= -90 && y <= 90 
+    return x >= -180 && x <= 180 && y >= -180 && y <= 180
   }
 }
