d82f5b0ffdd70067a620d940ea5ce50fb49b1356
vcosmin2701 <38065643+vcosmin2701@users.noreply.github.com>
2024-09-08 19:41:09 +0000

    [refactor] - Print statements modified

--- App.swift
+++ App.swift
@@ -13,14 +13,14 @@ class App {
 
   func run(arguments: [String]) {
       guard arguments.count == 4 else {
-          print("Please provide user coordinates (X Y) and a valid file path.")
+          print("Please provide user coordinates (X Y) and a valid file path/url.")
           exit(1)
       }
 
       guard let userXCoordinate = Double(arguments[1]),
           let userYCoordinate = Double(arguments[2]),
           coordinateValidator.isValid(x: userXCoordinate, y: userYCoordinate) else {
-        print("Invalid coordinates. Please provide valid numbers for the user coordinates (X Y) within the range (-180 to 180 for X, -90 to 90 for Y).")
+        print("Invalid coordinates. Please provide valid numbers for the user coordinates (X Y) within the range (-180 to 180 for X & Y.")
         exit(1)
     }
 
